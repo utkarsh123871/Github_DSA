fca20b25dc0f6bc9666739495d730f59aca0030f
UTKARSH GUPTA <72146426+utkarsh123871@users.noreply.github.com>
2022-06-05 11:39:52 +0530

    Create practice.cpp

---
+++ Blocks/Functions_2/practice.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int cal_trailing(int n){
+    //formula
+    int i = 1 , zeroes = 0;
+    while(i<=n/2){
+        zeroes += n/pow(5 , i);
+        i++;
+    }
+    return zeroes;
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout << cal_trailing(n);
+    return 0;
+}
