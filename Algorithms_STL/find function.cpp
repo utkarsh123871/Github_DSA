941b69b5b197c6938b42f55f7c8ad870a671b9d3
UTKARSH GUPTA <72146426+utkarsh123871@users.noreply.github.com>
2022-06-13 23:20:37 +0530

    Create find function.cpp

---
+++ Algorithms_STL/find function.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    
+    int arr[] = {5 , 4 , 7 , 6 , 0 , 1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    int key;
+    cin>>key;
+    
+    int *ans = find(arr , arr+n , key);
+    
+    int index = ans - arr;  // this will return the index at which ans is present 
+                          // subtracting the address gives us the index of ans address bucket
+
+    if(index == n){
+        std::cout << "Key is not found" << std::endl;
+    }
+    
+    else{
+        std::cout << "Key is found at index " << index <<std::endl;
+    }
+    return 0;
+}
