0a1aff7010324bb4304a3f96b35763800a9470cb
UTKARSH GUPTA <72146426+utkarsh123871@users.noreply.github.com>
2022-06-15 12:34:00 +0530

    Create bitwise_operators.cpp

---
+++ bitmasking/bitwise_operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<< (5 & 7) << endl;  //AND
+    cout<< (5 | 7) << endl;  //OR
+    cout<< (5 ^ 7) << endl;  //XOR
+    return 0;
+}
