3a3f3dcac82fe9acba8ce82e2d795566edd90561
UTKARSH GUPTA <72146426+utkarsh123871@users.noreply.github.com>
2022-06-16 12:20:38 +0530

    Create credit_score.cpp

---
+++ CodeChef/credit_score.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int X;
+	cin>>X;
+	if(X>=750){
+	    std::cout << "YES" << std::endl;
+	}
+	else{
+	    std::cout << "NO" << std::endl;
+	}
+	return 0;
+}
