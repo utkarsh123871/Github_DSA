b2b9bdde57693aba33e3446b78e9fc88c20c69d6
UTKARSH GUPTA <72146426+utkarsh123871@users.noreply.github.com>
2022-07-02 22:18:17 +0530

    Update Trailing_Zeroes.cpp

--- Blocks/Functions/Trailing_Zeroes.cpp
+++ Blocks/Functions/Trailing_Zeroes.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int cal_trailing(int n){
     //formula
     int i = 1 , zeroes = 0;
+    // while loop for the function
     while(i<=n/2){
         zeroes += n/pow(5 , i);
         i++;
