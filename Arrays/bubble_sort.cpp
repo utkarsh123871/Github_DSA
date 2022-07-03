cc380f2106bb97aeee9ae3666ca0ec736d813f2a
UTKARSH GUPTA <72146426+utkarsh123871@users.noreply.github.com>
2022-07-03 18:58:30 +0530

    Update bubble_sort.cpp

--- Arrays/bubble_sort.cpp
+++ Arrays/bubble_sort.cpp
@@ -10,7 +10,7 @@ int main()
     for (int i = 0; i < n; i++) {
         cout<<a[i]<<" ";
     }
-    
+    // swapping 
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j <= n-2 ; j++){
             if(a[j] > a[j+1]){
