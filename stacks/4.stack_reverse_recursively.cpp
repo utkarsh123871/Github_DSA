cc973e62635325ee053ff57e9d3049760c1ac512
UTKARSH GUPTA <72146426+utkarsh123871@users.noreply.github.com>
2022-07-17 12:40:04 +0530

    Update 4.stack_reverse_recursively.cpp

--- stacks/4.stack_reverse_recursively.cpp
+++ stacks/4.stack_reverse_recursively.cpp
@@ -2,52 +2,41 @@
 #include <stack>
 using namespace std;
 
-void tranfer(stack<int> &s1 , stack<int> &s2 , int n){
-    for (int i = 0; i < n; i++) {
-        s2.push(s1.top());
-        s1.pop();
+void insert(stack<int>&s , int element){
+    if(s.size()==0){
+        s.push(element);
+        return;
     }
+    
+    int temp = s.top();
+    s.pop(); 
+    insert(s,element);
+    s.push(temp);
 }
 
-
-void reverse_stack(stack<int> &s1){
-    // helper stack 
-    stack <int> s2;
-    int n = s1.size();
-    
-    for (int i = 0; i < n; i++) {
-        //pick the element at top and insert it at the bottom
-        int x = s1.top();
-        s1.pop();
-        
-        //tranfer n-i-1 elements from stack s1 to s2
-        tranfer(s1,s2,n-i-1);        
-        
-        //insert the element x in s1
-        s1.push(x);
-        
-        //tranfer n-i-1 elements from stack s2 to s1
-        tranfer(s2,s1,n-i-1);
+void reverse(stack<int>&s){
+    if(s.size()==1 or s.size()==0){
+        return;
     }
+    
+    int temp = s.top();
+    s.pop();
+    reverse(s);
+    insert(s,temp);
+    return;
 }
 
 int main()
 {
-    stack <int> s;
+    stack<int> s;
     s.push(1);
     s.push(2);
     s.push(3);
     s.push(4);
-    
-    // while(!s.empty()){
-    //     cout << s.top();
-    //     s.pop();
-    // }
-    
-    reverse_stack(s);
-    
+    s.push(5);
+    reverse(s);
     while(!s.empty()){
-        cout << s.top();
+        cout<<s.top()<<" ";
         s.pop();
     }
     return 0;
